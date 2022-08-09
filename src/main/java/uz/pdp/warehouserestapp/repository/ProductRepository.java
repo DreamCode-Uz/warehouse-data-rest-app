f7eb227accdf8a6c16652a3d5d5e7b8f335912da
Dilshod Fayzullayev <darkprohub-uz@yandex.com>
2022-08-09 17:51:51 +0500

    Initial commit

--- src/main/java/uz/pdp/warehouserestapp/repository/ProductRepository.java
+++ src/main/java/uz/pdp/warehouserestapp/repository/ProductRepository.java
@@ -2,8 +2,8 @@ package uz.pdp.warehouserestapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import uz.pdp.warehouserestapp.entity.Supplier;
+import uz.pdp.warehouserestapp.entity.Product;
 
 @RepositoryRestResource
-public interface ProductRepository extends JpaRepository<Supplier, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
